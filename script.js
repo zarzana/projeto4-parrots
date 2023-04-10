f165722e7df1e77314be0d711827811ac5a9dd31
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-04-10 17:53:30 -0300

    implemented game

--- script.js
+++ script.js
@@ -1,48 +1,124 @@
 document.addEventListener('DOMContentLoaded', init);
 
+var numOfTurns = 0;
+
 function init() {
 
+    const gameBoard = document.querySelector('.game-board');
     const cardOptions = [
         {name: 'bobrossparrot', img: './assets/bobrossparrot.gif'},
         {name: 'explodyparrot', img: './assets/explodyparrot.gif'},
         {name: 'fiestaparrot', img: './assets/fiestaparrot.gif'},
         {name: 'metalparrot', img: './assets/metalparrot.gif'},
         {name: 'revertitparrot', img: './assets/revertitparrot.gif'},
-        {name: 'tripletsparrot', img: './assets/tripletsparrot.gif'}
+        {name: 'tripletsparrot', img: './assets/tripletsparrot.gif'},
+        {name: 'unicornparrot', img: './assets/unicornparrot.gif'}
     ]
 
-    const game = document.querySelector('.game');
+    var cardAmount = 0;
 
-    generateGame(game, cardOptions);
+    while (!(cardAmount >= 4 && cardAmount <= cardOptions.length*2 && cardAmount % 2 == 0)) {
+        var cardAmount = prompt("Com quantas cartas gostaria de jogar?");
+    }
+
+    cardIds = fillGameBoard(gameBoard, cardAmount);
+    cardRelationship = createCardRelationship(cardAmount, cardOptions, cardIds);
+    
+    var hasFlipped = false;
+    var flippedCardName;
+    var flippedCardId;
+
+    flipCard(cardRelationship, hasFlipped, flippedCardName, flippedCardId);
 
 }
 
-function generateGame(game, cardOptions) {
+function createCardRelationship (cardAmount, cardOptions, cardIds) {
 
-    for (let i = 0; i < cardOptions.length; i++) {
+    var cardRelationship = [];
+    var cardArray = cardOptions.slice(0, cardAmount/2).concat(cardOptions.slice(0, cardAmount/2)).sort((a, b) => 0.5 - Math.random());
 
-        if (i == 3) {
-            var breakDiv = document.createElement('div');
-            breakDiv.setAttribute('class', 'break');
-            game.appendChild(breakDiv);
-        }
+    for (let i = 0; i < cardIds.length; i++) {
+        cardRelationship.push(Object.assign({},  {'id': cardIds[i]}, cardArray[i]));
+    }
+
+    return cardRelationship;
+
+}
+
+function fillGameBoard (gameBoard, cardAmount) {
+
+    var cardIds = [];
+
+    for (let i = 0; i < cardAmount; i++) {
+
+        let cardId = 'card' + i
+        cardIds.push(cardId)
 
         var card = document.createElement('div');
-        // card.setAttribute('id', 'card'+i);
+        card.setAttribute('id', cardId);
         card.setAttribute('class', 'card');
+        card.setAttribute('data-test', 'card');
 
         var cardBack = document.createElement('img');
+        cardBack.setAttribute('class', 'card-back');
         cardBack.setAttribute('src', './assets/back.png');
-
-        // var cardFront = document.createElement('img');
-        // cardFront.setAttribute('src', cardOptions[i].img);
+        cardBack.setAttribute('data-test', 'face-down-image');
 
         card.appendChild(cardBack);
-        //card.appendChild(cardFront);
-
-        game.appendChild(card);
-        game.appendChild(card.cloneNode(true));
+        gameBoard.appendChild(card);
 
     }
+
+    return cardIds;
+
+}
+
+function flipCard (cardRelationship, hasFlipped, flippedCardName, flippedCardId) {
+
+    cardRelationship.forEach(card => {
+
+        var currentCard = document.getElementById(card['id']);
+
+        currentCard.addEventListener('click', () => {
+
+            numOfTurns += 1;
+
+            var cardFront = document.createElement('img');
+            cardFront.setAttribute('class', 'card-front');
+            cardFront.setAttribute('src', card['img']);
+            cardFront.setAttribute('data-test', 'face-up-image');
+            currentCard.appendChild(cardFront);
+
+            // virar carta
+
+            if (hasFlipped) {
+
+                if (card['name'] == flippedCardName && card['id'] != flippedCardId) {
+                }
+
+                else {
+                    setTimeout(() => {
+                        // fechar cartas
+                        let cardToRemove = document.getElementsByClassName('card-front')[0];
+                        cardToRemove.parentNode.removeChild(cardToRemove);
+                        currentCard.removeChild(cardFront)
+                    }, 1000)
+                }
+
+                hasFlipped = false;
+                flippedCardName = null;
+                flippedCardId = null;
+                
+            }
+
+            else {
+                hasFlipped = true;
+                flippedCardName = card['name'];
+                flippedCardId = card['id'];
+            }
+
+        });
+
+    });
 
 }
